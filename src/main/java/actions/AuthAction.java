45477d6f6c7fd8800214fd9df2cff7f959736909
sinji-kojo <shinji_kojyou@jrass.jp>
2023-05-22 21:15:31 +0900

    ログアウトアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -32,7 +32,6 @@ public class AuthAction extends ActionBase {
 
         service.close();
     }
-
     /**
      * ログイン画面を表示する
      * @throws ServletException
@@ -95,5 +94,22 @@ public class AuthAction extends ActionBase {
             //ログイン画面を表示
             forward(ForwardConst.FW_LOGIN);
         }
+    }
+    /**
+     * ログアウト処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void logout() throws ServletException, IOException {
+
+        //セッションからログイン従業員のパラメータを削除
+        removeSessionScope(AttributeConst.LOGIN_EMP);
+
+        //セッションにログアウト時のフラッシュメッセージを追加
+        putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+
+        //ログイン画面にリダイレクト
+        redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+
     }
 }
